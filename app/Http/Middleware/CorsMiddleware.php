6e4aed45aaec5b7e9b9f273a5041b1f7c1e8e8c9
Ezequiel Avila Garcia <ezequielavilagarcia@gmail.com>
2017-10-17 21:57:25 -0300

    Trying solve cors problem

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -16,8 +16,9 @@ class corsMiddleware
     public function handle($request, Closure $next)
     {
         $response =  $next($request);
-        $response->header('Access-Control-Allow-Origin', '*')
-                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Origin', '*');
         return $response;
     }
 }
